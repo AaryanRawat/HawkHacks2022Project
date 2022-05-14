77f16c495e7c3200a79c67b72d82ca42a102380b
AaryanRawat <66914091+AaryanRawat@users.noreply.github.com>
2022-05-14 15:49:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.messages = True
 
-playlists = ['Romantic Movie Locations', 'Horror Movie Locations', 'Random Movie Locations']
-playlists_check = ['Romantic', 'Horror', 'Random']
+PLAYLISTS = ['Romantic Movie Locations', 'Horror Movie Locations', 'Random Movie Locations']
+PLAYLISTS_CHECK = [playlist.split()[0] for playlist in PLAYLISTS]
 
 bot = commands.Bot('geodude ')
 
@@ -37,12 +37,12 @@ async def commands(ctx):
 async def start(ctx):
     await ctx.send(
         'Select A Playlist:\n'
-        + '\n'.join(f'{i+1}. {playlist}' for i, playlist in enumerate(playlists)) +
+        + '\n'.join(f'{i+1}. {playlist}' for i, playlist in enumerate(PLAYLISTS)) +
         '\nType out the first word of playlist name, For eg. Romantic for Romantic Movie Locations'
     )
     def check(msg):
         if msg.author == ctx.author:
-            return any(msg.content == playlist for playlist in playlists_check)
+            return any(msg.content == playlist for playlist in PLAYLISTS_CHECK)
         return False
     try:
         msg = await bot.wait_for("message", check = check, timeout = 30)
@@ -53,4 +53,4 @@ async def start(ctx):
     except asyncio.TimeoutError:
         await ctx.channel.send('Selection took too long, please try again')
 
-bot.run('OTc0ODUzMjc2OTIxMTM1MTA0.G9CYa8.BBkD6vwHez-zU8s9TO4onrBXyUgzLqTA4VCZJg')
+bot.run('')
