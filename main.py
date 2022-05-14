e8cebc2d04177c53f75dc3853a31951bb701e00b
AaryanRawat <66914091+AaryanRawat@users.noreply.github.com>
2022-05-14 15:43:34 -0400

    switched to command wrapper for better use

--- main.py
+++ main.py
@@ -3,48 +3,54 @@ import asyncio
 import streetview as sv
 import os
 
+from discord.ext import commands
+
 intents = discord.Intents.default()
 intents.messages = True
-
-client = discord.Client(intents=intents)
 
 playlists = ['Romantic Movie Locations', 'Horror Movie Locations', 'Random Movie Locations']
 playlists_check = ['Romantic', 'Horror', 'Random']
 
-@client.event
+bot = commands.Bot('geodude ')
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    
+    await bot.process_commands(message)
 
-    if message.content.startswith('geodude-hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
 
-    if message.content.startswith('geodude-commands'):
-        await message.channel.send('geodude-hello')
-        await message.channel.send('geodude-commands')
-        await message.channel.send('geodude-start')
+@bot.command()
+async def commands(ctx):
+    comms = ['hello', 'commands', 'start']
+    await ctx.send((f'{com}' for com in enumerate(comms)) + '\n')
 
-    if message.content.startswith('geodude-start'):
-        await message.channel.send('Select A Playlist: ')
-        await message.channel.send(f'1. {playlists[0]} : ')
-        await message.channel.send(f'2. {playlists[1]}: ')
-        await message.channel.send(f'3. {playlists[2]}: ')
-        await message.channel.send('Type out the first word of playlist name, For eg. Romantic for Romantic Movie Locations')
-        def check(msg):
-            for item in playlists_check:
-                if(msg == item):
-                    return True
-        try:
-            msg = await client.wait_for("message", check = check, timeout = 30)
-            #start of the game where we need the google api stuff. probably need a class to store all the pulled data
-            #and do comparisons with user answers. 
-            await message.channel.send(f'You selected {msg} playlist!')
+@bot.command()
+async def start(ctx):
+    await ctx.send(
+        'Select A Playlist:\n'
+        + '\n'.join(f'{i+1}. {playlist}' for i, playlist in enumerate(playlists)) +
+        '\nType out the first word of playlist name, For eg. Romantic for Romantic Movie Locations'
+    )
+    def check(msg):
+        if msg.author == ctx.author:
+            return any(msg.content == playlist for playlist in playlists_check)
+        return False
+    try:
+        msg = await bot.wait_for("message", check = check, timeout = 30)
+        #start of the game where we need the google api stuff. probably need a class to store all the pulled data
+        #and do comparisons
+        await ctx.send(f'You selected {msg.content} playlist!')
 
-        except asyncio.TimeoutError:
-            await message.channel.send('Selection took too long, please try again')
+    except asyncio.TimeoutError:
+        await ctx.channel.send('Selection took too long, please try again')
 
-client.run('')
+bot.run('OTc0ODUzMjc2OTIxMTM1MTA0.G9CYa8.BBkD6vwHez-zU8s9TO4onrBXyUgzLqTA4VCZJg')
