1359aac914139c7c6092bfe7ae8fdc8ffb4ab7d2
Quinton Mak <qclockwork@gmail.com>
2022-05-14 20:30:41 -0400

    updated comments

--- streetview.py
+++ streetview.py
@@ -13,7 +13,8 @@ Note: Because of the way google_streetview API works, you can only access the mo
 class StreetView:
     """
     key (str): the API key from google streetview to use
-    dest (str): the file path in which to save the images 
+    dest (str) is the file FOLDER to save the image to. If the folder does not exist, it will be created. The file name will be 'gsv_0' as 
+        per the google streetview api. The default folder is 'images'.
     """
 
     def __init__(self, key, dest='images'):
@@ -26,19 +27,8 @@ class StreetView:
     Saves a jpg streetview image of the specified location.
 
     size (str) is the dimensions of the saved image file (jpg). Formatted as 'size': '600x300', max 640x640 pixels.
-    location (str) can be either a text string (such as Chagrin Falls, OH) or a lat/lng value (40.457375,-80.009353), 
-        and should be URL-encoded, so addresses such as "City Hall, New York, NY" should be converted to "City+Hall,New+York,NY",
-        before the request is signed. The Street View Static API will snap to the panorama photographed closest to this location.
-        When an address text string is provided, the API may use a different camera location to better display the specified location. 
-        When a lat/lng is provided, the API searches a 50 meter radius for a photograph closest to this location. 
-        Because Street View imagery is periodically refreshed, and photographs may be taken from slightly different positions each time,
-        it's possible that your location may snap to a different panorama when imagery is updated.
-    heading (str) indicates the compass heading of the camera. Formatted as '151.78'. Values range from 0 to 360.
-    pitch (str) specifies the up or down angle of the camera. Formatted as 'pitch': '-0.76'. Values range from -90 to 90.
-            'key': 'your_dev_key'
-        }]
     From Google Streeetview Static API: 
-        location can be either a text string (such as Chagrin Falls, OH) or a lat/lng value (40.457375,-80.009353), 
+        location (str) can be either a text string (such as Chagrin Falls, OH) or a lat/lng value (40.457375,-80.009353), 
         and should be URL-encoded, so addresses such as "City Hall, New York, NY" should be converted to "City+Hall,New+York,NY",
         before the request is signed. The Street View Static API will snap to the panorama photographed closest to this location.
         When an address text string is provided, the API may use a different camera location to better display the specified location. 
@@ -46,8 +36,10 @@ class StreetView:
         Because Street View imagery is periodically refreshed, and photographs may be taken from slightly different positions each time,
         it's possible that your location may snap to a different panorama when imagery is updated.
         Note: I don't think URL encoding is needed, a normal string should work fine for this API
-    dest (str) is the file FOLDER to save the image to. If the folder does not exist, it will be created. The file name will be 'gsv_0' as 
-        per the google streetview api.
+    heading (str) indicates the compass heading of the camera. Formatted as '151.78'. Values range from 0 to 360.
+    pitch (str) specifies the up or down angle of the camera. Formatted as 'pitch': '-0.76'. Values range from -90 to 90.
+            'key': 'your_dev_key'
+        }]
     """
 
     def saveLocation(self, size, location, heading, pitch):
